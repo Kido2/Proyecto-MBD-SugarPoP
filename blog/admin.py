1bdb23f687784b34e015ba9930ca18c0fddfd9e9
Nicolas Caicedo <64296100+nicolas0699@users.noreply.github.com>
2020-11-07 21:05:31 -0500

    Update admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Admin, User_sugar, Product, Domc, Client
+from .models import Admin, User_sugar, Product, Domc, Client, Method_of_payment, Fact, Shopping_cart, Product_cart, \
+    Client_cart, Client_cartfac, Gummy, Chocolate, Flowers, Box_of_chocolates, Box_of_gummies, Arrag_of_gummies, \
+    Arrag_of_chocls
 
-admin.site.register([Admin, User_sugar, Product, Domc, Client])
+admin.site.register([User_sugar, Admin, Client, Admin, User_sugar, Product, Domc, Client, Method_of_payment, Fact, 
+                     Shopping_cart, Product_cart, Client_cart, Client_cartfac, Gummy, Chocolate, Flowers, 
+                     Box_of_chocolates, Box_of_gummies, Arrag_of_gummies, Arrag_of_chocls])
 
 u1 = User_sugar(id_user=1, nickname='Nico', password='Nicoolas_123')
 u1.save()
