c5d87b2f9cea4d0bd87c93eb2b45c09764fe330e
SOFIA <sofiaroba2011@gmail.com>
2020-11-07 20:47:16 -0500

    correc2

--- blog/admin.py
+++ blog/admin.py
@@ -2,3 +2,6 @@ from django.contrib import admin
 from .models import Admin, User_sugar, Product, Domc, Client
 
 admin.site.register([Admin, User_sugar, Product, Domc, Client])
+
+u1 = User_sugar(id_user=1, nickname='Nico', password='Nicoolas_123')
+u1.save()
