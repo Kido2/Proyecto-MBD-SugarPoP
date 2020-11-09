afd6100e51e09cfc0d922541430aa8ae65f45576
Nicolas Caicedo <aycinenadiego@gmail.com>
2020-11-08 22:22:51 -0500

    aut v3

--- SugarPoP/settings.py
+++ SugarPoP/settings.py
@@ -127,3 +127,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
+
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
--- SugarPoP/urls.py
+++ SugarPoP/urls.py
@@ -20,4 +20,6 @@ from register import views as v
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', v.register, name="register"),
+    path('', include("blog.urls")),
+    path('', include('django.contrib.auth.urls')),
 ]
