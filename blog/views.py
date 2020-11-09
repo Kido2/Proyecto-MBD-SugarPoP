2c2f7dfb6755d9ce2e8e08f2ff6738be5e1aba80
Nicolas Caicedo <aycinenadiego@gmail.com>
2020-11-08 23:30:55 -0500

    aut v4

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Admin, Client, Domc
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login as do_login
 
 
 def home(request):
